60d9de849102f0621d5d956e44fd31b6a7b9acd1
thor <thor@thor-ThinkPad-Edge-E540>
2016-05-14 13:14:18 +0200

    Det virker nogenlunde nu

--- web/js/mainScript.js
+++ web/js/mainScript.js
@@ -29,7 +29,13 @@ function populateTruckList(){
     });
     
 }
-
+function clearFirebase(path){
+    
+    var ref = new Firebase("https://sizzling-heat-4676.firebaseio.com/"+ path);
+    ref.remove(function(){
+        console.log("data at " + path +"cleared");
+    });
+}
 function depopulateTruckList(){
     var div = document.getElementById('sensorTable');
     var section = document.getElementById('section');
@@ -37,33 +43,6 @@ function depopulateTruckList(){
     while(div.firstChild){
         div.removeChild(div.firstChild);
     }
-}
-
-function populateSensorList(){
-    var ref = new Firebase("https://sizzling-heat-4676.firebaseio.com");
-    
-    var tablearea = document.getElementById("sensorTable")
-    var table = document.createElement('table');
-    
-    ref.once("value", function (dataSnapshot){
-        console.log(dataSnapshot.val());
-        dataSnapshot.forEach(function(childSnapshot){
-            
-            
-            console.log(childSnapshot.key()); 
-        });
-    });
-    var tablearea = document.getElementById("sensorTable")
-    var table = document.createElement('table');
-    for(var i= 1; i < 4; i++ ){
-        console.log(i)
-        var row = tablearea.insertRow(-1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        cell1.innerHTML = "dlksfj "+ i;
-        cell2.innerHTML = "sdælkfjadslækfj " + i;
-        
-    }       
 }
 
 function setCookie(cname, cvalue, exdays) {
@@ -99,3 +78,88 @@ function checkCookie() {
         }
     }
 }
+
+// http://stackoverflow.com/questions/4744299/how-to-get-datetime-in-javascript-->
+        function AddZero(num) {
+            return (num >= 0 && num < 10) ? "0" + num : num + "";
+        }
+        
+        function getDate(){
+            var date =new Date();
+            
+            var strDate = [AddZero(date.getDate().toLocaleString()), AddZero(date.getMonth()+1), AddZero(date.getFullYear())].join("-");
+            return strDate;
+        }
+        
+        function getNodeSensors(truck_id){
+            document.getElementById("Truck_id").innerHTML = truck_id;
+            initMap();
+            console.log(getDate() + "--------------------------------------------")
+            var ref = new Firebase("https://sizzling-heat-4676.firebaseio.com/"+ truck_id +"/sensor/node_frontTruckSection/");
+//            var ref = new Firebase("https://sizzling-heat-4676.firebaseio.com/Truck_Thorm_0/sensor/node_frontTruckSection/");
+            console.log("firebase made");
+            ref.once('value', function (snap){
+                console.log("flflflflflflflflflflfl");
+                var sensorTypeAmount = snap.numChildren(); 
+                console.log(sensorTypeAmount);
+                snap.forEach(function (snapChild){
+                    console.log(snapChild.key().toString());
+                    var sensorType = snapChild.key().toString();
+                    switch(sensorType){
+                        case 'temp':
+                                console.log("setting up temperature table");
+                                getSensordata('temp', truck_id);
+                            break;
+                        case 'light':
+                            getSensordata('light', truck_id);
+                            break;
+                                
+                        case 'pressure':
+                            console.log("setting up pressure table");
+                            getSensordata('pressure', truck_id);
+                            break;
+                    }
+                        
+                });
+                    
+            });
+                
+        }        
+               
+        function getSensordata(sensorType, truck_id){
+            
+                console.log("ligth found in switch");
+            var ref = new Firebase("https://sizzling-heat-4676.firebaseio.com/"+ truck_id+"/sensor/node_frontTruckSection/"+sensorType);
+            
+            ref.once('value', function (lSnap){
+               var lightSensorCount = lSnap.numChildren();
+               console.log("number of " + sensorType + " sensors " + lightSensorCount);
+               for(var i = 0; i < lightSensorCount; i++){
+                   createSensorEntry(i, sensorType, truck_id);
+               }
+               
+            });
+            
+        }
+        
+        function createSensorEntry(index, sensorType, truck_id){
+            var curDate = getDate();
+            console.log("creating "+ sensorType + " table " + index);
+            var lightSensRef = new Firebase("https://sizzling-heat-4676.firebaseio.com/"+ truck_id+"/sensor/node_frontTruckSection/" + sensorType + "/"+ index +"/"+ curDate);
+        
+            var tablearea = document.getElementById(sensorType + "Table");
+            var row = tablearea.insertRow(-1);
+            var cell_truckUID = row.insertCell(0);
+//            var cell_truckStatus = row.insertCell(1);
+        
+        
+            lightSensRef.limitToLast(1).on('child_added', function(datasnapshot){
+                
+                var data = datasnapshot.val();
+                console.log(datasnapshot.val() + " its out of the loop");
+        
+                row.style.backgroundColor = "lightblue";
+                cell_truckUID.innerHTML = datasnapshot.val().toString();
+        
+            });
+        }
