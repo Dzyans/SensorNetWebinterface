7c8ded7fa15d7b3bfb99818c91af2d8bbb808eff
thor <thor@thor-ThinkPad-Edge-E540>
2016-05-15 18:28:17 +0200

    added nullguard on cookiechecker

--- src/java/utils/LoginChecker.java
+++ src/java/utils/LoginChecker.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package utils;
 
 import javax.servlet.http.Cookie;
@@ -15,26 +15,27 @@ import javax.servlet.http.Cookie;
 public class LoginChecker {
     
     
-  public static boolean checkLoggedIn(Cookie[] cookiesToBeChecked){
-        Cookie[] cookies = cookiesToBeChecked;
-             boolean loggedInCookieFound = false;
-             boolean loggedIn = false;
-             for(int i = 0; i < cookies.length; i++){
-                 System.out.print(cookies[i].getName());
-                 if(cookies[i].getName().equals("login")){
-                     System.out.println("login cookie found");
-                     loggedInCookieFound = true;
-                     Cookie c = cookies[i];
-                     System.out.println(c.getValue());
-                     if(!c.getValue().equals("valid")){
-                         System.out.println("found cookie, but not valid");
-                     return false;
-                 } else loggedIn = true;
-                 }                
-             }
-              if(loggedInCookieFound && loggedIn){
-                  return true;
-                 } else return false;    
+    public static boolean checkLoggedIn(Cookie[] cookiesToBeChecked){
+        if(cookiesToBeChecked != null){           
+            Cookie[] cookies = cookiesToBeChecked;
+            boolean loggedInCookieFound = false;
+            boolean loggedIn = false;
+            for(int i = 0; i < cookies.length; i++){
+                System.out.print(cookies[i].getName());
+                if(cookies[i].getName().equals("login")){
+                    System.out.println("login cookie found");
+                    loggedInCookieFound = true;
+                    Cookie c = cookies[i];
+                    System.out.println(c.getValue());
+                    if(!c.getValue().equals("valid")){
+                        System.out.println("found cookie, but not valid");
+                        return false;
+                    } else loggedIn = true;
+                }
+            }
+            if(loggedInCookieFound && loggedIn){
+                return true;
+            } else return false;
+        }else return false;
     }
-    
 }
